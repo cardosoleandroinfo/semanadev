c7b3d2e836ba0842a00cb877e09c515c16bcfee7
cardosoleandroinfo <cardosoleandroinfo@gmail.com>
2022-09-11 19:22:35 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,29 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../util/request';
 import './styles.css'
 
-function NotificationButton() {
+
+type Props = {
+    salesId:number;
+}
+
+function handleClick(id:number)
+{
+    axios (`${BASE_URL}/sales/${id}/notification`)
+    .then(response =>{
+        console.log("SUCESSO");
+        }
+    )
+    .catch(response=>{console.log("ERRO");
+}
+    )
+}
+
+function NotificationButton({salesId} : Props) {
     return (
         <>
-            <div className="dsmeta-red-btn">
+            <div className="dsmeta-red-btn" onClick={()=>handleClick(salesId)}>
                 <img src={icon} alt="Notificar" />
             </div>
         </>
